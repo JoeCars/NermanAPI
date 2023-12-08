af9c7de852e28c9a0aaef260c2708ab1d0f80fb4
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-12-08 14:36:37 -0800

    added single-user endpoint.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,39 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
+import User from "./schemas/User";
 
 const app = express();
 
 app.get("/", () => {
 	console.log("hello world");
+});
+
+app.get("/:discordId", async (req, res) => {
+	const discordId = req.params.discordId;
+	const user = await User.findOne({ discordId: discordId }).exec();
+
+	if (!user) {
+		return res.status(500).json({
+			message: "no user found."
+		});
+	}
+
+	const {
+		eligiblePolls: votesEligible,
+		participatedPolls: votesParticipated
+	}: { eligiblePolls: number; participatedPolls: number } = user.eligibleChannels.get(process.env.NOUNCIL_CHANNEL_ID!);
+
+	const participationRate = (votesParticipated / votesEligible) * 100;
+
+	const userStats = {
+		userId: discordId,
+		votesEligible,
+		votesParticipated,
+		participationRate
+	};
+
+	res.status(200).json(userStats);
 });
 
 app.use(() => {
