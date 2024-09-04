b51fcf228979637eb55468250eb4e49edfd19f75
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-09-04 12:49:38 -0700

    feat: add Nouncillor schema model

---
+++ src/schemas/Nouncillor.ts
@@ -0,0 +1,35 @@
+import { model, Schema, Types } from "mongoose";
+
+export interface INouncillor {
+	_id: Types.ObjectId;
+	discordId: string;
+	walletAddress: string;
+	twitterAddress: string;
+	farcasterAddress: string;
+	dateJoined: Date;
+}
+
+const nouncillorSchema = new Schema<INouncillor>({
+	discordId: {
+		type: Schema.Types.String,
+		required: true
+	},
+	walletAddress: {
+		type: Schema.Types.String,
+		required: true
+	},
+	twitterAddress: {
+		type: Schema.Types.String,
+		required: true
+	},
+	farcasterAddress: {
+		type: Schema.Types.String,
+		required: true
+	},
+	dateJoined: {
+		type: Schema.Types.Date,
+		required: false
+	}
+});
+
+export default model("Nouncillor", nouncillorSchema);
